b707dd85503ce349c9711ec19e7bed11cd711f16
Ben Muschol <benmuschol@gmail.com>
2017-05-18 21:01:49 -0400

    add bodyParser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser')
+
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
   res.render('index', { msg: 'Welcome to chorebot!' })
