4a5146545d47bc6019800fbf9f2e2d1995e4a41a
Ben Muschol <benmuschol@gmail.com>
2017-05-18 22:39:06 -0400

    all caps

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.render('index', { msg: 'Welcome to chorebot!' })
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 let server = app.listen(3000, () => {
   console.log(`Server running at localhost:${server.address().port}`)
 })
